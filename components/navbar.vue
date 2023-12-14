079207f2d3c8fae55c217ae9d038550a65ef46c5
Sami Ramadan <samyramadan.bts@gmail.com>
2023-12-14 10:55:57 +0200

    add nuxt link and navigateto

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,9 +1,7 @@
 <template>
-  <!-- NAVBAR -->
   <header
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
-    <a class="text-3xl" href="">cartrader</a>
+    <nuxt-link class="text-3xl" to="/">cartrader</nuxt-link>
   </header>
-  <!-- NAVBAR -->
 </template>
