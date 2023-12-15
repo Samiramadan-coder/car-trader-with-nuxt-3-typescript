78697f8ca2ca9c233b555b06fdf0256931ff6644
Sami Ramadan <samyramadan.bts@gmail.com>
2023-12-15 16:26:59 +0200

    revisiting the skipped functionality

--- components/car/sidebar.vue
+++ components/car/sidebar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
-    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+    <!-- Location -->
+    <div
+      class="p-5 flex justify-between relative cursor-pointer border-b space-x-2"
+    >
       <h3>Location</h3>
       <h3 @click="updateModal('location')" class="text-blue-400 capitalize">
         {{ route.params.city }}
@@ -20,21 +23,81 @@
       </div>
     </div>
 
+    <!-- Make -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{ route.params.make || "Any" }}
+      </h3>
+
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+        v-if="modal.make"
+      >
+        <h4
+          v-for="make in makes"
+          :key="make"
+          class="w-1/3"
+          @click="onChangeMake(make)"
+        >
+          {{ make }}
+        </h4>
+      </div>
     </div>
 
+    <!-- Price -->
     <div class="p-5 flex justify-between relative cursor-pointer">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">Any</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+        {{ priceRangeText }}
+      </h3>
+
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+        v-if="modal.price"
+      >
+        <input
+          type="number"
+          name=""
+          id=""
+          placeholder="Min"
+          v-model="priceRange.min"
+          class="border p-1 rounded"
+        />
+        <input
+          type="number"
+          name=""
+          id=""
+          placeholder="Max"
+          v-model="priceRange.max"
+          class="border p-1 rounded mt-2"
+        />
+        <button
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+          @click="onPriceChange"
+        >
+          Apply
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute();
+const router = useRouter();
 const city = ref<string>("");
+const { makes } = useCars();
+
+interface PriceRange {
+  min: string;
+  max: string;
+}
+
+const priceRange = ref<PriceRange>({
+  min: "",
+  max: "",
+});
 
 interface Modal {
   make: boolean;
@@ -49,6 +112,20 @@ const modal = ref<Modal>({
 });
 
 type Key = "make" | "location" | "price";
+
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+
+  if (!minPrice && !maxPrice) return "Any";
+  else if (!minPrice && maxPrice) {
+    return `< ${maxPrice}`;
+  } else if (minPrice && !maxPrice) {
+    return `> ${minPrice}`;
+  } else {
+    return `$${minPrice} - $${maxPrice}`;
+  }
+});
 
 const updateModal = (key: Key): void => {
   modal.value[key] = !modal.value[key];
@@ -67,5 +144,25 @@ const onChangeLocation = (): void => {
   updateModal("location");
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = "";
+};
+
+const onChangeMake = (make: string): void => {
+  updateModal("make");
+  navigateTo(`/city/${route.params.city}/car/${make}`);
+};
+
+const onPriceChange = (): void => {
+  updateModal("price");
+
+  if (priceRange.value.max && priceRange.value.min) {
+    if (priceRange.value.min > priceRange.value.max) return;
+  }
+
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max,
+    },
+  });
 };
 </script>
--- composables/useCars.ts
+++ composables/useCars.ts
@@ -1,10 +1,14 @@
 import listOfCars from "~/data/cars.json";
+import listOfMakes from "~/data/makes.json";
+
 import type { Car } from "~/helpers/interfaces";
 
 export const useCars = () => {
   const cars: Car[] = listOfCars;
+  const makes: string[] = listOfMakes;
 
   return {
     cars,
+    makes,
   };
 };
